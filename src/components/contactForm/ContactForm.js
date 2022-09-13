b46ad5eae01e587856b326dbb34e661e356d9ad9
Matthew-Mays <matthew.c.mays96@gmail.com>
2022-09-13 17:05:58 -0500

    add input form to ContactsPage

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,10 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+      <input type='tel' value={phone} onChange={(e) => setPhone(e.target.value)} />
+      <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+    </form>
   );
 };
